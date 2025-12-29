81a2d5be113df1f002c30e17bed0011020332d28
brendoncc <45137600+brendoncc@users.noreply.github.com>
2025-12-29 18:29:10 +0000

    Fix: Remove tracked files that are now in .gitignore

--- Debug/Core/Src/cli.d
+++
@@ -1,4 +0,0 @@
-Core/Src/cli.o: ../Core/Src/cli.c ../Core/Inc/cli.h \
- ../Core/Inc/cli_defs.h
-../Core/Inc/cli.h:
-../Core/Inc/cli_defs.h:
--- Debug/Core/Startup/startup_stm32g0b1cbtx.d
+++
@@ -1,2 +0,0 @@
-Core/Startup/startup_stm32g0b1cbtx.o: \
- ../Core/Startup/startup_stm32g0b1cbtx.s
